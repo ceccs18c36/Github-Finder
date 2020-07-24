7d938e7ac59db39087c050762a37825b270725a0
jesswinw <jesswinw@gmail.com>
2020-07-24 23:38:53 +0530

    changed to functional components😁

---
+++ src/components/Users/Search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <form action=''>
+            <input type='text' name='Query' id='' />
+            <button type='submit' className='btn btn-dark btn-block'>
+                Search
+            </button>
+        </form>
+    );
+};
+
+export default Search;
