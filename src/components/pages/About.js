1d0f66b711fb0c1872c6a65deae8641b9b88ef87
jesswinw <jesswinw@gmail.com>
2020-07-27 01:40:45 +0530

    Added Links to Navbar and new About page😁

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = (props) => {
+    return (
+        <div>
+            <h1>Github Finder</h1>
+            <p>An App to fetch the details of Github Profiles</p>
+            <p>Version 1.0.0</p>
+        </div>
+    );
+};
+
+export default About;
