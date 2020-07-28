d6fef29b3b2ad830b77641887efea8a1dc7d8124
jesswinw <jesswinw@gmail.com>
2020-07-28 23:36:56 +0530

    added some code with User🙂

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -1,31 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
     }
+    static propTypes = {
+        loading: PropTypes.bool.isRequired,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired,
+    };
     render() {
-        const {
-            name,
-            bio,
-            avatar_url: avatar,
-            login,
-            followers,
-            following,
-            repos_url,
-        } = this.props.user;
-        for (let item of [
-            name,
-            bio,
-            avatar,
-            login,
-            followers,
-            following,
-            repos_url,
-        ]) {
-            console.log(item);
-        }
-        return <div>Hello World</div>;
+        const { hireable, avatar_url, login, bio } = this.props.user;
+        const { loading } = this.props;
+
+        if (loading) return <Spinner />;
+        return (
+            <Fragment>
+                <Link to='/' className='btn btn-light'>
+                    <i className='fa fa-arrow-circle-left'></i> Back to Search
+                </Link>
+                Hireable:{' '}
+                {hireable ? (
+                    <i className='fa fa-check text-success'></i>
+                ) : (
+                    <i className='fa fa-times text-danger '></i>
+                )}
+                <div className='card grid-2'>
+                    <div className='all-center'>
+                        <img
+                            src={avatar_url}
+                            alt='Avatar'
+                            className='round-img'
+                            style={{ width: '150px' }}
+                        />
+                        <h3>{login}</h3>
+                        <p>{bio}</p>
+                    </div>
+                </div>
+            </Fragment>
+        );
     }
 }
 
