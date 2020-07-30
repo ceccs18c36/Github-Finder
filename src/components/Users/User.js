89ad2d4adc01f555e13cab573b6400e5952797ad
jesswinw <jesswinw@gmail.com>
2020-07-30 15:09:56 +0530

    More items to the user component:)

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -4,45 +4,99 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class User extends Component {
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login);
-    }
-    static propTypes = {
-        loading: PropTypes.bool.isRequired,
-        user: PropTypes.object.isRequired,
-        getUser: PropTypes.func.isRequired,
-    };
-    render() {
-        const { hireable, avatar_url, login, bio } = this.props.user;
-        const { loading } = this.props;
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
+  render() {
+    const {
+      hireable,
+      avatar_url,
+      html_url,
+      blog,
+      login,
+      company,
+      location,
+      bio,
+      public_gists,
+      public_repos,
+      followers,
+      following,
+    } = this.props.user;
+    const { loading } = this.props;
 
-        if (loading) return <Spinner />;
-        return (
-            <Fragment>
-                <Link to='/' className='btn btn-light'>
-                    <i className='fa fa-arrow-circle-left'></i> Back to Search
-                </Link>
-                Hireable:{' '}
-                {hireable ? (
-                    <i className='fa fa-check text-success'></i>
-                ) : (
-                    <i className='fa fa-times text-danger '></i>
+    if (loading) return <Spinner />;
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          <i className='fa fa-arrow-circle-left'></i> Back to Search
+        </Link>
+        Hireable:{' '}
+        {hireable ? (
+          <i className='fa fa-check text-success'></i>
+        ) : (
+          <i className='fa fa-times text-danger '></i>
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              alt='Avatar'
+              className='round-img'
+              style={{ width: '150px' }}
+            />
+            <h3>{login}</h3>
+            {location && <p>Location:: {location}</p>}
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio:</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit on Github
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username:</strong> {login}
+                  </Fragment>
                 )}
-                <div className='card grid-2'>
-                    <div className='all-center'>
-                        <img
-                            src={avatar_url}
-                            alt='Avatar'
-                            className='round-img'
-                            style={{ width: '150px' }}
-                        />
-                        <h3>{login}</h3>
-                        <p>{bio}</p>
-                    </div>
-                </div>
-            </Fragment>
-        );
-    }
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong>
+                    {blog}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company:</strong> {company}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default User;
