190bd19523781da4f63b7b766d74c484a889a5e7
jesswinw <jesswinw@gmail.com>
2020-07-30 23:25:49 +0530

    small change in style

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,16 +1,16 @@
-import React, { Fragment } from 'react';
-import spinner from './Spinner.gif';
+import React, { Fragment } from "react";
+import spinner from "./Spinner.gif";
 
 const Spinner = () => {
-    return (
-        <Fragment>
-            <img
-                src={spinner}
-                alt='Loading...'
-                style={{ width: '50px', margin: 'auto', display: 'block' }}
-            />
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <img
+        src={spinner}
+        alt="Loading..."
+        style={{ width: "60px", margin: "auto", display: "block" }}
+      />
+    </Fragment>
+  );
 };
 
 export default Spinner;
