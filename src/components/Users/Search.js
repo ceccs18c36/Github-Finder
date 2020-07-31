4228593e2db3b0c7673225247d6c83328a0f6a3f
jesswinw <jesswinw@gmail.com>
2020-07-31 20:20:00 +0530

    converted to a functional component

--- src/components/Users/Search.js
+++ src/components/Users/Search.js
@@ -1,59 +1,48 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-class Search extends Component {
-    state = {
-        name: '',
-    };
+const Search = ({ fireAlert, findUser, showClear, clearUsers }) => {
+  const [name, setName] = useState("");
 
-    static propTypes = {
-        findUser: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        fireAlert: PropTypes.func.isRequired,
-    };
-
-    onchange = (e) => this.setState({ [e.target.name]: e.target.value });
-
-    find = (e) => {
-        e.preventDefault();
-        if (this.state.name === '') {
-            this.props.fireAlert(
-                'Please Enter the username before searching',
-                'info'
-            );
-        } else {
-            this.props.findUser(this.state.name);
-            this.setState({ name: '' });
-        }
-    };
-
-    render() {
-        const { showClear, clearUsers } = this.props;
-        return (
-            <form className='form' onSubmit={this.find}>
-                <input
-                    type='text'
-                    name='name'
-                    placeholder='Enter the username'
-                    value={this.state.name}
-                    onChange={this.onchange}
-                />
-                <button type='submit' className='btn btn-dark btn-block'>
-                    Search
-                </button>
-                {showClear && (
-                    <button
-                        className='btn btn-clear btn-block'
-                        type='submit'
-                        onClick={clearUsers}
-                    >
-                        Clear
-                    </button>
-                )}
-            </form>
-        );
+  const find = (e) => {
+    e.preventDefault();
+    if (name === "") {
+      fireAlert("Please Enter the username before searching", "info");
+    } else {
+      findUser(name);
+      setName("");
     }
-}
+  };
+  return (
+    <form className="form" onSubmit={find}>
+      <input
+        type="text"
+        name="name"
+        placeholder="Enter the username"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button type="submit" className="btn btn-dark btn-block">
+        Search
+      </button>
+      {showClear && (
+        <button
+          className="btn btn-clear btn-block"
+          type="submit"
+          onClick={clearUsers}
+        >
+          Clear
+        </button>
+      )}
+    </form>
+  );
+};
+
+Search.propTypes = {
+  findUser: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  fireAlert: PropTypes.func.isRequired,
+};
 
 export default Search;
--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -1,107 +1,106 @@
-import React, { Component, Fragment } from 'react';
-import Spinner from '../layout/Spinner';
-import Repo from '../Repos/Repo';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React, { useEffect, Fragment } from "react";
+import Spinner from "../layout/Spinner";
+import Repo from "../Repos/Repo";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getRepo(this.props.match.params.login);
-  }
-  static propTypes = {
-    loading: PropTypes.bool.isRequired,
-    user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getRepo: PropTypes.func.isRequired,
-    repos: PropTypes.array.isRequired,
-  };
-  render() {
-    const {
-      hireable,
-      avatar_url,
-      html_url,
-      blog,
-      login,
-      company,
-      location,
-      bio,
-      public_gists,
-      public_repos,
-      followers,
-      following,
-    } = this.props.user;
-    const { loading, repos } = this.props;
+const User = ({ user, getUser, getRepo, loading, repos, match }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getRepo(match.params.login);
+    //eslint-disable-next-line
+  }, []);
 
-    if (loading) return <Spinner />;
-    return (
-      <Fragment>
-        <Link to='/' className='btn btn-light'>
-          <i className='fa fa-arrow-circle-left'></i> Back to Search
-        </Link>
-        Hireable:{' '}
-        {hireable ? (
-          <i className='fa fa-check text-success'></i>
-        ) : (
-          <i className='fa fa-times text-danger '></i>
-        )}
-        <div className='card grid-2'>
-          <div className='all-center'>
-            <img
-              src={avatar_url}
-              alt='Avatar'
-              className='round-img'
-              style={{ width: '150px' }}
-            />
-            <h3>{login}</h3>
-            {location && <p>Location:: {location}</p>}
-          </div>
-          <div>
-            {bio && (
-              <Fragment>
-                <h3>Bio:</h3>
-                <p>{bio}</p>
-              </Fragment>
-            )}
-            <a href={html_url} className='btn btn-dark my-1'>
-              Visit on Github
-            </a>
-            <ul>
-              <li>
-                {login && (
-                  <Fragment>
-                    <strong>Username:</strong> {login}
-                  </Fragment>
-                )}
-              </li>
-              <li>
-                {blog && (
-                  <Fragment>
-                    <strong>Website: </strong>
-                    {blog}
-                  </Fragment>
-                )}
-              </li>
-              <li>
-                {company && (
-                  <Fragment>
-                    <strong>Company:</strong> {company}
-                  </Fragment>
-                )}
-              </li>
-            </ul>
-          </div>
+  const {
+    hireable,
+    avatar_url,
+    html_url,
+    blog,
+    login,
+    company,
+    location,
+    bio,
+    public_gists,
+    public_repos,
+    followers,
+    following,
+  } = user;
+
+  if (loading) return <Spinner />;
+  return (
+    <Fragment>
+      <Link to="/" className="btn btn-light">
+        <i className="fa fa-arrow-circle-left"></i> Back to Search
+      </Link>
+      Hireable:{" "}
+      {hireable ? (
+        <i className="fa fa-check text-success"></i>
+      ) : (
+        <i className="fa fa-times text-danger "></i>
+      )}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            alt="Avatar"
+            className="round-img"
+            style={{ width: "150px" }}
+          />
+          <h3>{login}</h3>
+          {location && <p>Location:: {location}</p>}
         </div>
-        <div className='card text-center'>
-          <div className='badge badge-primary'>Followers: {followers}</div>
-          <div className='badge badge-success'>Following: {following}</div>
-          <div className='badge badge-light'>Public Repos: {public_repos}</div>
-          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        <div>
+          {bio && (
+            <Fragment>
+              <h3>Bio:</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a href={html_url} className="btn btn-dark my-1" target="blank">
+            Visit on Github
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <Fragment>
+                  <strong>Username:</strong> {login}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <Fragment>
+                  <strong>Website: </strong>
+                  {blog}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {company && (
+                <Fragment>
+                  <strong>Company:</strong> {company}
+                </Fragment>
+              )}
+            </li>
+          </ul>
         </div>
-        <Repo repos={repos} />
-      </Fragment>
-    );
-  }
-}
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-success">Following: {following}</div>
+        <div className="badge badge-light">Public Repos: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      </div>
+      <Repo repos={repos} />
+    </Fragment>
+  );
+};
 
+User.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  user: PropTypes.object.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getRepo: PropTypes.func.isRequired,
+  repos: PropTypes.array.isRequired,
+};
 export default User;
