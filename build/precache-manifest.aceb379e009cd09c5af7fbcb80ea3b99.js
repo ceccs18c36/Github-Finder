87d32425319f2526a3d7d9e74fa3f24c371dc054
jesswinw <jesswinw@gmail.com>
2020-08-03 21:03:47 +0530

    Build folder

---
+++ build/precache-manifest.aceb379e009cd09c5af7fbcb80ea3b99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa15134c67cc723741974f9448b23d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efc97971a414d19d071c",
+    "url": "/static/css/main.faef0c8f.chunk.css"
+  },
+  {
+    "revision": "e189c2dba44b10a2b671",
+    "url": "/static/js/2.ecb283b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ecb283b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc97971a414d19d071c",
+    "url": "/static/js/main.dc6414d1.chunk.js"
+  },
+  {
+    "revision": "25d48e391914dc08b45e",
+    "url": "/static/js/runtime-main.d89612ff.js"
+  },
+  {
+    "revision": "584b607f5c2ff075429dc0e7b8d142ef",
+    "url": "/static/media/Spinner.584b607f.gif"
+  }
+]);
